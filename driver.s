11e9bdb635a924a95646f24ada269bc8abe15bf2
jaezu <66060223+jaezudev@users.noreply.github.com>
2022-11-26 22:55:08 +0100

    add driver source code
    very unfinished 💀

---
+++ driver.s
@@ -0,0 +1,14 @@
+; Basic-ass SID sound driver
+; don't question why
+
+; first of all, we need to define some stuff
+; cuz that will make our lives easier
+
+define	freqlo1	$d400
+define	freqhi1	$d401
+define	cr1	$d404
+define	ad1	$d405
+define	sr1	$d406
+define	vol	$d418
+
+; TODO: code the actual routine
