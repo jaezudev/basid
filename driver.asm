3f8d794588c39a89273cedd5d847fe8d8dbae341
jaezu <66060223+jaezudev@users.noreply.github.com>
2022-11-27 18:54:05 +0100

    add some more stuff

--- driver.asm
+++ driver.asm
@@ -6,9 +6,26 @@
 
 define	freqlo1	$d400
 define	freqhi1	$d401
-define	cr1	$d404
-define	ad1	$d405
-define	sr1	$d406
-define	vol	$d418
+define	cr1		$d404
+define	ad1		$d405
+define	sr1		$d406
+define	vol		$d418
 
 ; TODO: code the actual routine
+
+; select waveform (10h = triangle)
+lda #$10
+sta cr1
+
+; set ADSR (3, 7, 0, 0)
+lda #$37
+sta ad1
+lda #$00
+sta sr1
+
+; divide frequency into high and low
+
+
+; play
+lda #$0f
+sta vol
