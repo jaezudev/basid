30775b77f07d356d89aaac7f4c8bff9e5af11db0
jaezu <66060223+jaezudev@users.noreply.github.com>
2022-11-30 17:30:39 +0100

    Create music.asm

---
+++ music.asm
@@ -0,0 +1,4 @@
+#importonce 
+.var ch1Data = List().add(48, 50, 52, 53, 55, 57, 59, 60, 255).lock()
+.var ch2Data = List().add(52, 53, 55, 57, 59, 60, 62, 64, 255).lock()
+.var ch3Data = List().add(55, 57, 59, 60, 62, 64, 66, 67, 255).lock()
