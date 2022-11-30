4079afa9629ee46561a54de8fd6b1ad89932a571
jaezu <66060223+jaezudev@users.noreply.github.com>
2022-11-30 13:25:30 +0100

    checkpoint, kinda
    this is still WIP

--- driver.asm
+++ driver.asm
@@ -1,5 +1,8 @@
 *=$1000
 
+// import the music data
+#import "music.asm"
+
 // CH1 definitions
 .label freqlo1=$d400
 .label freqhi1=$d401
@@ -31,8 +34,15 @@
 .label vol=$d418
 
 // Notetable
-.var loBytes = List().add(22, 39, 57, 75, 95, 116, 138, 161, 186, 212, 240, 14, 45, 78, 113, 150, 190, 231, 20, 66, 116, 169, 224, 27, 90, 156, 226, 45, 123, 207, 39, 133, 232, 81, 193, 55, 180, 56, 196, 89, 247, 158, 78, 10, 208, 162, 129, 109, 103, 112, 137, 178, 237, 59, 157, 20, 160, 69, 3, 219, 207, 225, 18, 101, 219, 118, 58, 39, 65, 138, 5, 181, 157, 193, 36, 201, 182, 237, 115, 78, 130, 20, 10, 106, 59, 130, 72, 147, 107, 218, 231, 156, 4, 40, 20)
-.var hiBytes = List().add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 12, 13, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 26, 27, 29, 31, 32, 34, 36, 39, 41, 43, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 124, 131, 139, 147, 156, 165, 175, 185, 196, 208, 221, 234, 248)
+.var loBytes = List().add(22, 39, 57, 75, 95, 116, 138, 161, 186, 212, 240, 14, 45, 78, 113, 150, 190, 231, 20, 66, 116, 169, 224, 27, 90, 156, 226, 45, 123, 207, 39, 133, 232, 81, 193, 55, 180, 56, 196, 89, 247, 158, 78, 10, 208, 162, 129, 109, 103, 112, 137, 178, 237, 59, 157, 20, 160, 69, 3, 219, 207, 225, 18, 101, 219, 118, 58, 39, 65, 138, 5, 181, 157, 193, 36, 201, 182, 237, 115, 78, 130, 20, 10, 106, 59, 130, 72, 147, 107, 218, 231, 156, 4, 40, 20, 0)
+.var hiBytes = List().add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 12, 13, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 26, 27, 29, 31, 32, 34, 36, 39, 41, 43, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 124, 131, 139, 147, 156, 165, 175, 185, 196, 208, 221, 234, 248, 0)
+
+// Miscellaneous stuff
+.var note = 0
+.var w1 = $11
+.var w2 = $11
+.var w3 = $11
+.var i = 0
 
 start:
 		// max out the volume
@@ -47,22 +57,32 @@ start:
 		sta sr1
 		sta sr2
 		sta sr3
+
+loop:
 		// frequency lo-byte and hi-byte
-		ldx #loBytes.get(48)
-		ldy #hiBytes.get(48)
+		.eval note = ch1Data.get(i)
+		ldx #loBytes.get(note)
+		ldy #hiBytes.get(note)
 		stx freqlo1
 		sty freqhi1
-		ldx #loBytes.get(52)
-		ldy #hiBytes.get(52)
+		.eval note = ch2Data.get(i)
+		ldx #loBytes.get(note)
+		ldy #hiBytes.get(note)
 		stx freqlo2
 		sty freqhi2
-		ldx #loBytes.get(55)
-		ldy #hiBytes.get(55)
+		.eval note = ch3Data.get(i)
+		ldx #loBytes.get(note)
+		ldy #hiBytes.get(note)
 		stx freqlo3
 		sty freqhi3
-		// set waveform (triangle = $11)
-		lda #$11
+		// set waveform
+		lda #w1
+		ldx #w2
+		ldy #w3
 		sta wave1
-		sta wave2
-		sta wave3
-		rts
+		stx wave2
+		sty wave3
+		.eval i++
+		.if (i==ch1Data.size()) {
+			rts
+		}
